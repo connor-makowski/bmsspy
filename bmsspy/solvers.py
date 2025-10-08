4f00b45a5215cc8a384a7b7be0e41bdc9205524c
Connor Makowski <connorman528@gmail.com>
2025-10-08 16:17:40 -0400

    Allow sets to be passed as origin_id to solvers.bmssp

--- bmsspy/solvers.py
+++ bmsspy/solvers.py
@@ -4,7 +4,7 @@ from .helpers.utils import input_check, reconstruct_path
 
 def bmssp(
     graph: list[dict[int, int | float]],
-    origin_id: int,
+    origin_id: int | set[int],
     destination_id: int = None,
 ):
     """
@@ -20,8 +20,9 @@ def bmssp(
     - `graph`:
         - Type: list of dictionaries
     - `origin_id`
-        - Type: int
+        - Type: int | set of int
         - What: The id of the origin node from the graph dictionary to start the shortest path from
+        - Note: If you pass a set, only the first id in the set will be checked for input validation
     - `destination_id`
         - Type: int | None
         - What: The id of the destination node from the graph dictionary to end the shortest path at
@@ -35,15 +36,21 @@ def bmssp(
     Returns:
 
     - A dictionary with the following keys
-        - `origin_id`: The id of the origin node
+        - `origin_id`: The id of the origin node or a list of ids if a set was provided
         - `destination_id`: The id of the destination node (or None)
         - `predecessor`: The predecessor list for path reconstruction
         - `distance_matrix`: The distance matrix from the origin node to all other nodes
         - `path`: The shortest path from origin_id to destination_id (or None)
         - `length`: The length of the shortest path from origin_id to destination_id (or None)
     """
+    if isinstance(origin_id, set):
+        if len(origin_id) < 1:
+            raise ValueError("Your provided origin_id set must have at least 1 node")
+        origin_id_check = next(iter(origin_id))
+    else:
+        origin_id_check = origin_id
     # Input Validation
-    input_check(graph=graph, origin_id=origin_id, destination_id=destination_id)
+    input_check(graph=graph, origin_id=origin_id_check, destination_id=destination_id)
     # Run the BMSSP Algorithm to relax as many edges as possible.
     solver = BmsspSolver(graph, origin_id)
     if destination_id is not None:
@@ -53,7 +60,7 @@ def bmssp(
             )
 
     return {
-        "origin_id": origin_id,
+        "origin_id": origin_id if isinstance(origin_id, int) else list(origin_id),
         "destination_id": destination_id,
         "predecessor": solver.predecessor,
         "distance_matrix": solver.distance_matrix,
